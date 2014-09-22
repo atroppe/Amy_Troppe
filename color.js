6616c588760e67799736a6f8776157b0d24c5d6e
atroppe <amytroppe@amys-mbp.gateway.2wire.net>
2014-09-21 22:44:04 -0500

    more about me and logo

--- color.js
+++ color.js
@@ -4,7 +4,9 @@ function app() {
     console.log("app is working");
 
     $('.cameleon').css("backgroundColor", '#3544A4');
-    
+    $('.lineOne').fadeOut(750, function() {
+        $(this).html('vision').fadeIn(750);
+    });
 
     var cameleon = document.querySelector('.cameleon');
     var github = document.querySelector('.github');
@@ -17,12 +19,42 @@ function app() {
     // var popUp = document.querySelector('.popUp');
     // var reset = document.querySelector('.reset');
 
-    home.addEventListener('mouseover', changeColor('#E0125E'));
+    home.addEventListener('mouseover', changeColor('#BECF2C'));
+    home.addEventListener('mouseover', function() {
+        $('.lineOne').fadeOut(750, function() {
+            $(this).html('engineer').fadeIn(750);
+        });
+    });
     about.addEventListener('mouseover', changeColor('#922179'));
+    about.addEventListener('mouseover', function() {
+        $('.lineOne').fadeOut(750, function() {
+            $(this).html('improve').fadeIn(750);
+        });
+    });
     github.addEventListener('mouseover', changeColor('#1C1241'));
+    github.addEventListener('mouseover', function() {
+        $('.lineOne').fadeOut(750, function() {
+            $(this).html('code').fadeIn(750);
+        });
+    });
     work.addEventListener('mouseover', changeColor('#3544A4'));
-    resume.addEventListener('mouseover', changeColor('#BECF2C'));
+    work.addEventListener('mouseover', function() {
+        $('.lineOne').fadeOut(750, function() {
+            $(this).html('create').fadeIn(750);
+        });
+    });
+    resume.addEventListener('mouseover', changeColor('#E0125E'));
+    resume.addEventListener('mouseover', function() {
+        $('.lineOne').fadeOut(750, function() {
+            $(this).html('grow').fadeIn(750);
+        });
+    });
     contact.addEventListener('mouseover', changeColor('#FD9B32'));
+    contact.addEventListener('mouseover', function() {
+        $('.lineOne').fadeOut(750, function() {
+            $(this).html('forward').fadeIn(750);
+        });
+    });
     logo.addEventListener('mouseover', reset());
     logo.addEventListener('mouseout', function() {
         $('.cameleon').css("backgroundColor", '#3544A4');
@@ -38,6 +70,9 @@ function app() {
     function reset() {
         return function() {
             cameleon.style.backgroundColor = "transparent";
+            $('.lineOne').fadeOut(750, function() {
+                $(this).html('design').fadeIn(750);
+            });
         }
     }
 
